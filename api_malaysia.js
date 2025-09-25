c20b0f3361cb05f9bac2e8ec26ec9df54f32a33c
william22288 <142523309+william22288@users.noreply.github.com>
2025-09-26 00:44:43 +0800

    Fix comment syntax and log Vega view object

--- api_malaysia.js
+++ api_malaysia.js
@@ -1,5 +1,6 @@
 var vg_1 = "homeworkWeek9.json";
 vegaEmbed("#symbol_map", vg_1).then(function(result) {
-// Access the Vega view instance
-(https://vega.github.io/vega/docs/api/view/) as result.view
+  // Access the Vega view instance
+  var view = result.view; 
+  console.log("Vega view object:", view);
 }).catch(console.error);
